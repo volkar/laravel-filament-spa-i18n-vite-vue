d99cf2e81c8c94d34935f2c503bf489160f8d159
Sergey Volkar <sergey@volkar.ru>
2023-03-17 04:02:47 +0300

    Fixed vite build issues

---
+++ public/build/assets/AboutView-515d936f.js
@@ -0,0 +1,1 @@
+import{d as a,c,k as o,g as t,l as n}from"./app-2d0a6c10.js";const p=a({__name:"AboutView",setup(r){const e="pages/about";return(s,l)=>(t(),c("article",null,[(t(),o(n,{url:e,key:e}))]))}});export{p as default};
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,7 +34,7 @@
 
     <div id="app"></div>
 
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.ts'])
 
 </body>
 </html>
